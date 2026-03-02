51b40948eda13f25f752b83100cde6cb883c10f4
getnishan <nishan.fig@gmail.com>
2026-03-02 18:23:51 +0530

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,12 @@
+project = 'Medhavi Mayurbhnaj'
+author = 'Nishan'
+release = '0.1'
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'sphinx_rtd_theme'
+
+html_static_path = ['_static']
